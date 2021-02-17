408447022b82f04a01ee9443e83e2e4f0afd046f
MoonTahoe <alex@moonhighway.com>
2021-02-17 12:01:51 -0800

    this scope sample starter

--- 01-object-oriented-js/prototypal-inheritance/sample.js
+++ 01-object-oriented-js/prototypal-inheritance/sample.js
@@ -1,1 +1,13 @@
-console.log("ready");
+function Vacation(destination, length) {
+  this.destination = destination;
+  this.length = length;
+  this.inProgress = false;
+  this.backpackers = [];
+}
+
+const desoTrip = new Vacation("Desolation Wilderness", 10);
+
+console.log(desoTrip);
+console.log(typeof desoTrip);
+console.log(typeof Vacation);
+console.log(desoTrip instanceof Vacation);
--- 01-object-oriented-js/this-scope/sample.js
+++ 01-object-oriented-js/this-scope/sample.js
@@ -1,1 +1,11 @@
-console.log("ready");
+const lostCoastTrip = {
+  destination: "Lost Coast",
+  length: 3,
+  backpackers: ["Gene", "Phil", "Lou"],
+};
+
+const desolationTrip = {
+  destination: "Desolation Wilderness",
+  length: 10,
+  backpackers: ["Kathy"],
+};
