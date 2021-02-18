c053eac7aa708b710396971ebb092e494be4b57d
MoonTahoe <alex@moonhighway.com>
2021-02-17 17:16:25 -0800

    Adds samples for prototype inheritance

--- 01-object-oriented-js/prototypal-inheritance/sample.js
+++ 01-object-oriented-js/prototypal-inheritance/sample.js
@@ -1,13 +1,9 @@
-function Vacation(destination, length) {
-  this.destination = destination;
-  this.length = length;
-  this.inProgress = false;
-  this.backpackers = [];
-}
+const now = new Date();
 
-const desoTrip = new Vacation("Desolation Wilderness", 10);
+console.log(typeof now);
+console.log(typeof Date);
 
-console.log(desoTrip);
-console.log(typeof desoTrip);
-console.log(typeof Vacation);
-console.log(desoTrip instanceof Vacation);
+console.log(typeof Window);
+console.log(typeof Object);
+console.log(typeof Function);
+console.log(typeof Array);
