8d71be55f26f2f2f5e576ef808e4b682192832cf
MoonTahoe <alex@moonhighway.com>
2021-03-11 14:08:23 -0800

    Final Walk through of solution

--- 03-js-modules/building-javascript/solution/src/server.js
+++ 03-js-modules/building-javascript/solution/src/server.js
@@ -1,23 +1,16 @@
 import express from "express";
-import weather from "weather-js";
-import { promisify } from "util";
-
-const findWeather = promisify(weather.find);
+import { weatherSearch } from "./lib.js";
 
 const app = express();
 
 app.use(express.static("public"));
-app.get("/:city/:state", weatherSearch);
+
+app.get("/:city/:state", async (req, res) => {
+  const { city, state } = req.params;
+  const result = await weatherSearch(city, state);
+  res.send(result);
+});
+
 app.listen(3000, () =>
   console.log(`Weather Bug Running - http://localhost:3000`)
 );
-
-async function weatherSearch(req, res) {
-  const { city, state } = req.params;
-  const result = await findWeather({
-    search: `${city}, ${state}`,
-    degreeType: "F",
-  });
-
-  res.send(result);
-}
